5e8851f4ef9cbf9a850b5d95ca093d9fb91b02a4
rodcar <ivanrod12@hotmail.com>
2020-12-25 19:02:33 -0500

    Cambia credenciales 4

--- detail.php
+++ detail.php
@@ -3,7 +3,7 @@
 require __DIR__ .  '/vendor/autoload.php';
 
 // Agrega credenciales
-MercadoPago\SDK::setAccessToken('APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a 2082e-677408439');
+MercadoPago\SDK::setAccessToken('APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439');
 MercadoPago\SDK::setIntegratorId("​dev_2e4ad5dd362f11eb809d0242ac130004");
 
 // Crea un objeto de preferencia
